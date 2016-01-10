b50c66cb2b919356a8e9ac3f8553df5724ef9c02
jonathan <jonatghanbeaudoin1996@hotmail.com>
2016-01-10 16:00:18 -0500

    Updated ptrace

--- src/main/java/com/beaudoin/jmm/Main.java
+++ src/main/java/com/beaudoin/jmm/Main.java
@@ -1,16 +1,33 @@
 package com.beaudoin.jmm;
 
-import com.beaudoin.jmm.process.NativeProcess;
+import com.beaudoin.jmm.natives.linux.ptrace;
+import com.sun.jna.LastErrorException;
+import com.sun.jna.Memory;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
 
 /**
  * Created by Jonathan on 12/22/2015.
  */
 public final class Main {
 
-	public static void main(String... args) {
-		int processid = 7062;
-        NativeProcess process = NativeProcess.byName("");
+    private static final int PTRACE_PEEKDATA = 2;
+    private static final int PTRACE_ATTACH = 16;
+    private static final int PTRACE_DETACH = 17;
 
-	}
+    public static void main(String[] args) {
+        int pid = 1291;
+
+        try {
+            ptrace.ptrace(PTRACE_ATTACH, pid, null, null);
+            Memory buffer = new Memory(4);
+            ptrace.ptrace(PTRACE_PEEKDATA, pid, new Pointer(0x7f22465700L), buffer);
+            System.out.println(buffer.getInt(0));
+        } catch (LastErrorException ex) {
+            ex.printStackTrace();
+        } finally {
+            ptrace.ptrace(PTRACE_DETACH, pid, null, null);
+        }
+    }
 
 }
--- src/main/java/com/beaudoin/jmm/natives/linux/ptrace.java
+++ src/main/java/com/beaudoin/jmm/natives/linux/ptrace.java
@@ -1,7 +1,18 @@
 package com.beaudoin.jmm.natives.linux;
 
+import com.sun.jna.*;
+
 /**
  * Created by jonathan on 06/01/16.
  */
 public class ptrace {
+
+    static {
+        Native.register(NativeLibrary.getInstance("c"));
+    }
+
+    public static native long ptrace(/*enum __ptrace_request*/int request,
+            /*pid_t*/ int pid,
+                                     Pointer addr,
+                                     Pointer data) throws LastErrorException;
 }
