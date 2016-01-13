91d7e176824060baebd1b031893e73c50b3fb8df
Jonathan Beaudoin <jonathanbeaudoin1996@hotmail.com>
2016-01-12 21:42:33 -0500

    Added new Utils class for various methods

---
+++ src/main/java/com/beaudoin/jmm/misc/Utils.java
@@ -0,0 +1,20 @@
+package com.beaudoin.jmm.misc;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by Jonathan on 1/11/16.
+ */
+public final class Utils {
+
+    public static int exec(String... command) {
+        try {
+            return Integer.parseInt(new Scanner(Runtime.getRuntime().exec(command).getInputStream()).next());
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read output from " + Arrays.toString(command));
+        }
+    }
+
+}
