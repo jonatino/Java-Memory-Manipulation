18dfc2d09d993be70ba9889cdeec51c314738c24
Jonatino <jonathanbeaudoin1996@gmail.com>
2016-07-20 16:03:38 -0400

    Cached byte arrays

--- src/main/java/com/beaudoin/jmm/process/DataSource.java
+++ src/main/java/com/beaudoin/jmm/process/DataSource.java
@@ -12,84 +12,84 @@ import static com.beaudoin.jmm.misc.Cacheable.buffer;
  */
 public interface DataSource {
 
-    MemoryBuffer read(Pointer address, int size);
+	MemoryBuffer read(Pointer address, int size);
 
-    Process write(Pointer address, MemoryBuffer buffer);
+	Process write(Pointer address, MemoryBuffer buffer);
 
-    boolean canRead(Pointer address, int size);
+	boolean canRead(Pointer address, int size);
 
-    default boolean readBoolean(long address) {
-        return read(address, 1).getBoolean();
-    }
+	default boolean readBoolean(long address) {
+		return read(address, 1).getBoolean();
+	}
 
-    default int readByte(long address) {
-        return read(address, 1).getByte();
-    }
+	default int readByte(long address) {
+		return read(address, 1).getByte();
+	}
 
-    default int readShort(long address) {
-        return read(address, 2).getShort();
-    }
+	default int readShort(long address) {
+		return read(address, 2).getShort();
+	}
 
-    default int readInt(long address) {
-        return read(address, 4).getInt();
-    }
+	default int readInt(long address) {
+		return read(address, 4).getInt();
+	}
 
-    default long readUnsignedInt(long address) {
-        return Integer.toUnsignedLong(read(address, 4).getInt());
-    }
+	default long readUnsignedInt(long address) {
+		return Integer.toUnsignedLong(read(address, 4).getInt());
+	}
 
-    default long readLong(long address) {
-        return read(address, 8).getLong();
-    }
+	default long readLong(long address) {
+		return read(address, 8).getLong();
+	}
 
-    default float readFloat(long address) {
-        return read(address, 4).getFloat();
-    }
+	default float readFloat(long address) {
+		return read(address, 4).getFloat();
+	}
 
-    default double readDouble(long address) {
-        return read(address, 8).getDouble();
-    }
+	default double readDouble(long address) {
+		return read(address, 8).getDouble();
+	}
 
-    default String readString(long address, int length) {
-        byte[] bytes = new byte[length];
-        read(address, bytes.length).get(bytes);
-        return Strings.transform(bytes);
-    }
+	default String readString(long address, int length) {
+		byte[] bytes = Cacheable.array(length);
+		read(address, bytes.length).get(bytes);
+		return Strings.transform(bytes);
+	}
 
-    default MemoryBuffer read(long address, int size) {
-        return read(Cacheable.pointer(address), size);
-    }
+	default MemoryBuffer read(long address, int size) {
+		return read(Cacheable.pointer(address), size);
+	}
 
-    default Process writeBoolean(long address, boolean value) {
-        return write(Cacheable.pointer(address), buffer(1).putBoolean(value));
-    }
+	default Process writeBoolean(long address, boolean value) {
+		return write(Cacheable.pointer(address), buffer(1).putBoolean(value));
+	}
 
-    default Process writeByte(long address, int value) {
-        return write(Cacheable.pointer(address), buffer(1).putByte(value));
-    }
+	default Process writeByte(long address, int value) {
+		return write(Cacheable.pointer(address), buffer(1).putByte(value));
+	}
 
-    default Process writeShort(long address, int value) {
-        return write(Cacheable.pointer(address), buffer(2).putShort(value));
-    }
+	default Process writeShort(long address, int value) {
+		return write(Cacheable.pointer(address), buffer(2).putShort(value));
+	}
 
-    default Process writeInt(long address, int value) {
-        return write(Cacheable.pointer(address), buffer(4).putInt(value));
-    }
+	default Process writeInt(long address, int value) {
+		return write(Cacheable.pointer(address), buffer(4).putInt(value));
+	}
 
-    default Process writeLong(long address, long value) {
-        return write(Cacheable.pointer(address), buffer(8).putLong(value));
-    }
+	default Process writeLong(long address, long value) {
+		return write(Cacheable.pointer(address), buffer(8).putLong(value));
+	}
 
-    default Process writeFloat(long address, float value) {
-        return write(Cacheable.pointer(address), buffer(4).putFloat(value));
-    }
+	default Process writeFloat(long address, float value) {
+		return write(Cacheable.pointer(address), buffer(4).putFloat(value));
+	}
 
-    default Process writeDouble(long address, double value) {
-        return write(Cacheable.pointer(address), buffer(8).putDouble(value));
-    }
+	default Process writeDouble(long address, double value) {
+		return write(Cacheable.pointer(address), buffer(8).putDouble(value));
+	}
 
-    default boolean canRead(long address, int size) {
-        return canRead(Cacheable.pointer(address), size);
-    }
+	default boolean canRead(long address, int size) {
+		return canRead(Cacheable.pointer(address), size);
+	}
 
 }
