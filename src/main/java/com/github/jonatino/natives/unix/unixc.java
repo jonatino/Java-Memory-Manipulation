8ab68bcf64e69ee11e97a4483d6f2d5cce4f90d1
ericek111 <eb.skola@gmail.com>
2017-03-16 22:57:41 +0100

    Include native library
    If running from a JAR, extract the native library, otherwise use native java.library.path.

--- src/main/java/com/github/jonatino/natives/unix/unixc.java
+++ src/main/java/com/github/jonatino/natives/unix/unixc.java
@@ -1,13 +1,24 @@
 package com.github.jonatino.natives.unix;
 
+import java.io.IOException;
+
+import com.github.jonatino.misc.NativeUtils;
 import com.sun.jna.*;
 
 public final class unixc {
 
-	static {
+	static {		   
 		try {
-	    	System.loadLibrary("native_mem");
-	    } catch (UnsatisfiedLinkError e) {
+			String className = unixc.class.getName().replace('.', '/');
+			String classJar = unixc.class.getResource("/" + className + ".class").toString();
+			if (classJar.startsWith("jar:") && classJar.contains("!")) {
+				// Running from a JAR - extracting libnative_mem.so
+				NativeUtils.loadLibraryFromJar("/libnative_mem.so");
+			} else {
+				// Loading directly from java.library.path
+				System.loadLibrary("native_mem");
+			}
+	    } catch (UnsatisfiedLinkError | IOException e) {
 	      System.err.println("Native library (native_mem - libnative_mem.so) failed to load!\n");
 	      e.printStackTrace();
 	      System.exit(1);
